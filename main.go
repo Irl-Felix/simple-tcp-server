3b4271f18d8794b02cb3e9837488aa589f20a7f8
Felix <javifsk@gmail.com>
2025-05-24 13:31:07 +0800

    server init layout

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func healthCheckHandler(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Service is healthy"))
+	fmt.Println("Health check endpoint hit")
+}
 func main() {
 	fmt.Println("Hello, World!")
+	http.HandleFunc("/", healthCheckHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
